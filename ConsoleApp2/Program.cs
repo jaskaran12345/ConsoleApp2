a56e2581904bc0211bd19f8fc1d838f07fe77dbf
visualstudio <visualstudio@230-PC19>
2019-04-18 15:47:01 -0400

    jhyt

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
 
-namespace ConsoleApp2
+namespace ConsoleApp1
 {
+    class Program
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int[] arr = { 17, 11, 9, 3, 4, 11, 6, 11, 8, 7 };
+            int arr_size = arr.Length;
+
+            printRepeating(arr, arr_size);
+        }
+        static void printRepeating(int[] arr, int size)
+        {
+            int i, j;
+
+            Console.WriteLine("Repeated Elements are :");
+            for (i = 0; i < size; i++)
+            {
+                for (j = i + 1; j < size; j++)
+                {
+                    if (arr[i] == arr[j])
+                        Console.WriteLine(arr[i] + " ");
+                    Console.ReadLine();
+                }
+            }
         }
     }
 }
+
